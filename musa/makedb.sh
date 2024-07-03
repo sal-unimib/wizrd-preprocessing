f61f04892852815f0d2107991a73b44fbaee23b5
Jacopo Maltagliati <j.maltagliati@campus.unimib.it>
2024-07-03 11:59:49 +0200

    Modify makedb script to generate a db compatible with mapserver

--- musa/makedb.sh
+++ musa/makedb.sh
@@ -1,28 +1,58 @@
 #!/usr/bin/bash
 
 MAP_FILE=map-ciao.osm
-CONFIG_FILE=green-mapconfig.xml
+CONFIG_FILE=mapconfig.xml
 
 PGSQL_SERVER_ADDR=172.18.0.2
-PGSQL_DB_NAME=osm-pesi
+PGSQL_DB_NAME=mapserver
 
-export PGPASSWORD=admin
-dropdb --force -U admin -h $PGSQL_SERVER_ADDR --echo $PGSQL_DB_NAME
-createdb -U admin -h $PGSQL_SERVER_ADDR --echo $PGSQL_DB_NAME
+PGSQL_USER=admin
+PGSQL_PASS=admin
 
-RETURN=$?
+panic() {
+  RETURN=$1
 
-if [ $RETURN -ne 0 ];
-then
-  echo "ERROR: psql: failed to clear DB"
-  exit $RETURN
-fi 
+  if [ $RETURN -ne 0 ]; then
+    echo FATAL: $2
+    exit $RETURN
+  fi
+}
 
+export PGPASSWORD=$PGSQL_PASS
+
+echo "*** Phase 1 - Recreating DB from scratch... ***"
+dropdb --force -U $PGSQL_USER -h $PGSQL_SERVER_ADDR --echo $PGSQL_DB_NAME
+createdb -U $PGSQL_USER -h $PGSQL_SERVER_ADDR --echo $PGSQL_DB_NAME
+panic $? "psql: failed to recreate DB"
+
+echo "*** Phase 2 - Importing data from OSM dump file... ***"
 ../build/./osm2pgrouting \
     -f $MAP_FILE \
     -c $CONFIG_FILE \
     --dbname $PGSQL_DB_NAME \
-    -U admin \
-    -W admin \
+    -U $PGSQL_USER \
+    -W $PGSQL_PASS \
     -h $PGSQL_SERVER_ADDR \
-    --tags 
+    --tags \
+    --attributes
+
+echo "Renaming columns..."
+psql -U $PGSQL_USER -h $PGSQL_SERVER_ADDR -d $PGSQL_DB_NAME -c 'ALTER TABLE ways RENAME COLUMN gid TO id;'
+panic $? "psql: failed to rename column: ways.gid -> ways.id"
+
+echo "*** Phase 3 - Adding required extensions... ***"
+psql -U $PGSQL_USER -h $PGSQL_SERVER_ADDR -d $PGSQL_DB_NAME -c 'CREATE EXTENSION pgRouting CASCADE'
+panic $? "psql: failed to create extension: pgRouting"
+
+echo "*** Phase 4 - Importing functions... ***"
+COUNT=0
+
+for FUNC in functions/*.sql; do
+  echo Processing $FUNC...
+  psql -U $PGSQL_USER -h $PGSQL_SERVER_ADDR -d $PGSQL_DB_NAME -f $FUNC
+  panic $? "psql: failed to add function to DB"
+  ((COUNT=COUNT+1))
+done
+
+echo Imported $COUNT functions.
+
