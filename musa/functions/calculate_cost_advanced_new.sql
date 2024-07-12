79c9ace11207e15f7e7074c4eaa3fef93905a613
oet93 <m.vaghi9@campus.unimib.it>
2024-07-12 16:21:32 +0200

    Preprocessing traffico.

--- musa/functions/calculate_cost_advanced_new.sql
+++ musa/functions/calculate_cost_advanced_new.sql
@@ -5,8 +5,9 @@ CREATE OR REPLACE FUNCTION public.calculate_cost_advanced_new(
 	road_type text, 
 	pm2 integer, 
 	pm10 integer, 
+    traffic integer,
 	w_green double precision, 
-	safe_param_active boolean, 
+	safe_param_active boolean,
 	traffic_param boolean, 
 	distance_param boolean,
 	air_pollution_param boolean, 
@@ -20,6 +21,7 @@ DECLARE
     safe_param numeric := 1; -- Declare a variable to store the safe_param
     pm2_param numeric; -- Declare a variable to store the pm2_param
     pm10_param numeric; -- Declare a variable to store the pm10_param
+    traffic numeric := 1.0; --
     green_param_value numeric := 1.0;
     worse_pm_param numeric := 1.0;
 BEGIN
@@ -39,14 +41,14 @@ BEGIN
             WHEN road_type = 'footway' THEN 0.7
             WHEN road_type = 'residential' THEN 0.8
             WHEN road_type = 'track' THEN 0.7
-            WHEN road_type = 'steps' THEN 0.7 AND profile_type = 'pedestrian' THEN 0.7
+            WHEN road_type = 'steps' AND profile_type = 'pedestrian' THEN 0.7
             WHEN road_type = 'cycleway' AND profile_type = 'micromobility' THEN 0.7
             WHEN road_type = 'cycleway' THEN 0.8
             ELSE 1
         END;
     END IF;
     
-    -- Handle air pollution path param
+    -- Handle air pollution param
     IF air_pollution_param IS TRUE THEN
         pm2_param := ROUND(pm2 / 500.0, 2);
         IF pm2_param < 0.01 THEN pm2_param := 0.01; END IF;
@@ -57,6 +59,13 @@ BEGIN
         worse_pm_param := GREATEST(pm2_param, pm10_param); -- The worst air quality value
     END IF;
     
+    -- Handle traffic param
+    IF traffic_param IS TRUE THEN
+        traffic := 1.00 - ROUND(traffic / 4.0, 2);
+        IF traffic < 0.01 THEN traffic := 0.01; END IF;
+        RAISE NOTICE 'traffic: %, traffic_param: %', traffic, traffic_param;
+    END IF;
+
     --- parametro relativo alla preferenza di percorsi verdi
     IF green_param = TRUE THEN
         green_param_value := w_green;
@@ -67,7 +76,7 @@ BEGIN
     -- RAISE NOTICE 'slope source_elevation: %', source_elevation;
     -- RAISE NOTICE 'slope target_elevation: %', target_elevation;
 
-    edge_weight := edge_weight * safe_param * green_param_value * worse_pm_param;
+    edge_weight := (edge_weight / traffic) * safe_param * green_param_value * worse_pm_param;
 
     RETURN edge_weight;
 END;
