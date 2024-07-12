3ee7009a03a2564c3df77bea83428c22d67dab94
Jacopo Maltagliati <j.maltagliati@campus.unimib.it>
2024-07-12 15:14:09 +0200

    Pass map data through osmconvert to strip useless info

--- getmap-bicocca.sh
+++ getmap-bicocca.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+MAP_FILE=bicocca.osm
+
 # ~8MB
 BB_BOTTOM_LEFT=9.202734,45.506527
 BB_TOP_RIGHT=9.223731,45.527366
@@ -14,31 +16,14 @@ panic() {
 }
 
 hash curl 2>/dev/null 
-panic $? "curl is required to run this script"
+panic $? "\"curl\" is required to run this script"
 
-curl -o bicocca-$(date -I).osm https://overpass-api.de/api/map?bbox=$BB_BOTTOM_LEFT,$BB_TOP_RIGHT
+hash osmconvert 2>/dev/null 
+panic $? "\"osmconvert\" is required to run this script"
 
-# [jm] the following script extracts a portion of the map geofabrik
-#      it does not snip the green areas outside the bounding box correctly, 
-#      for some reason
-# hash osmconvert 2>/dev/null 
-# panic $? "\"osmconvert\" not found"
+mkdir -p .cache/
 
-# mkdir -p .cache
-# pushd .cache
-# echo "*** Downloading PBF map dump Italy/Nord Ovest from geofabrik.de... ***"
-# wget http://download.geofabrik.de/europe/italy/nord-ovest-latest.osm.pbf
-# wget http://download.geofabrik.de/europe/italy/nord-ovest-latest.osm.pbf.md5
-# echo "*** Validating PBF map dump... ***"
-# md5sum -c nord-ovest-latest.osm.pbf.md5
-# panic $? "md5sum: dump file bad"
-# echo "*** Bounding and converting to OSM... ***"
-# osmconvert nord-ovest-latest.osm.pbf \
-#     -b=$BB_BOTTOM_LEFT,$BB_TOP_RIGHT \
-#     --complete-ways \
-#     --drop-author \
-#     --drop-version \
-#     --out-osm \
-#     -o=../milano-$(date -I).osm
-# popd
-# rm -r .cache
+curl -o .cache/temp.osm https://overpass-api.de/api/map?bbox=$BB_BOTTOM_LEFT,$BB_TOP_RIGHT
+osmconvert .cache/temp.osm --complete-ways --drop-author --drop-version --out-osm -o=$MAP_FILE
+
+rm -r .cache/
