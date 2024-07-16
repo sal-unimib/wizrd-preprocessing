af561ef20e10d7088ee7d4f8955870d8e630fa32
Jacopo Maltagliati <j.maltagliati@campus.unimib.it>
2024-07-16 17:34:49 +0200

    Fix traffic->traffic_param assignment in CCA and use less ambiguous names for variables
    (cherry picked from commit 265ddf932ac265b2aae20034083a16acaab42e6c)

--- functions/calculate_cost_advanced_new.sql
+++ functions/calculate_cost_advanced_new.sql
@@ -4,36 +4,37 @@ CREATE OR REPLACE FUNCTION public.calculate_cost_advanced_new(
     time_cost numeric, 
     road_type text, 
     pm2 integer, 
-    pm10 integer, 
+    pm10 integer,
+    traffic integer,
     green boolean, 
-    safe_param_active boolean, 
-    traffic_param boolean, 
-    distance_param boolean, 
-    air_pollution_param boolean, 
-    green_param boolean)
+    is_safe boolean, 
+    is_traffic boolean, 
+    is_distance boolean, 
+    is_air_quality boolean, 
+    is_green boolean)
  RETURNS numeric
  LANGUAGE plpgsql
 AS $function$
 DECLARE
-    edge_weight numeric; -- Declare a variable to store the edge_weight
-    -- distance_influence numeric := 1;
-    safe_param numeric := 1; -- Declare a variable to store the safe_param
-    pm2_param numeric; -- Declare a variable to store the pm2_param
-    pm10_param numeric; -- Declare a variable to store the pm10_param
-    traffic numeric := 1.0; --
-    green_param_value numeric := 1.0;
+    -- Local variables
+    edge_weight numeric;
+    safe_param numeric := 1.0;
+    pm2_param numeric := 1.0;
+    pm10_param numeric := 1.0;
     worse_pm_param numeric := 1.0;
+    traffic_param numeric := 1.0; 
+    green_param numeric := 1.0;
 BEGIN
 
-    --- default algorithm
-    IF distance_param = TRUE THEN
+    -- default algorithm
+    IF is_distance = TRUE THEN
         edge_weight := edge_distance;
     ELSE
     	edge_weight := time_cost;
     END IF;
 
-    --- refract better this "safe_param" in a way to include diffrent type of profile
-    IF safe_param_active IS TRUE THEN
+    -- refract better this "safe_param" in a way to include diffrent type of profile
+    IF is_safe IS TRUE THEN
         safe_param := CASE
             WHEN road_type = 'pedestrian' THEN 0.7
             WHEN road_type = 'living_street' THEN 0.8
@@ -43,12 +44,18 @@ BEGIN
             WHEN road_type = 'steps' AND profile_type = 'pedestrian' THEN 0.7
             WHEN road_type = 'cycleway' AND profile_type = 'micromobility' THEN 0.7
             WHEN road_type = 'cycleway' THEN 0.8
-            ELSE 1
+            ELSE 1.0
         END;
+    END IF;
+
+    -- Handle traffic param
+    IF is_traffic IS TRUE THEN
+        traffic_param := 1.0 - ROUND(traffic / 4.0, 2);
+        IF traffic_param < 0.01 THEN traffic_param := 0.01; END IF;
     END IF;
     
     -- Handle air pollution param
-    IF air_pollution_param IS TRUE THEN
+    IF is_air_quality IS TRUE THEN
         pm2_param := ROUND(pm2 / 500.0, 2);
         IF pm2_param < 0.01 THEN pm2_param := 0.01; END IF;
         pm10_param := ROUND(pm10 / 500.0, 2);
@@ -57,26 +64,25 @@ BEGIN
         RAISE NOTICE 'pm10: %, pm10_param: %', pm10, pm10_param;
         worse_pm_param := GREATEST(pm2_param, pm10_param); -- The worst air quality value
     END IF;
-    
-    -- Handle traffic param
-    IF traffic_param IS TRUE THEN
-        traffic := 1.00 - ROUND(traffic / 4.0, 2);
-        IF traffic < 0.01 THEN traffic := 0.01; END IF;
-        RAISE NOTICE 'traffic: %, traffic_param: %', traffic, traffic_param;
-    END IF;
 
-    --- parametro relativo alla preferenza di percorsi verdi
-    IF green_param IS TRUE AND green IS TRUE THEN
+    -- parametro relativo alla preferenza di percorsi verdi
+    IF is_green IS TRUE AND green IS TRUE THEN
         -- TODO [jm] Weight based on surface type?
-        green_param_value := 0.5;
-        -- RAISE NOTICE 'Green param: %', green_param_value;
+        green_param := 0.5;
     END IF;
 
+    RAISE NOTICE 'edge_weight before: %', edge_weight;
+    RAISE NOTICE 'traffic_param: %', traffic_param;
+    RAISE NOTICE 'safe_param: %', safe_param;
+    RAISE NOTICE 'green_param: %', green_param;
+    RAISE NOTICE 'worse_pm_param: %', worse_pm_param;
     -- RAISE NOTICE 'slope value: %', slope;
     -- RAISE NOTICE 'slope source_elevation: %', source_elevation;
     -- RAISE NOTICE 'slope target_elevation: %', target_elevation;
 
-    edge_weight := (edge_weight / traffic) * safe_param * green_param_value * worse_pm_param;
+    edge_weight := (edge_weight / traffic_param) * safe_param * green_param * worse_pm_param;
+
+    RAISE NOTICE 'edge_weight after: %', edge_weight;
 
     RETURN edge_weight;
 END;
