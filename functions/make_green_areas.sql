acdb1eb7f10174718e47979c48582fce1bf28b7e
Jacopo Maltagliati <j.maltagliati@campus.unimib.it>
2024-10-31 19:50:50 +0100

    Reformat SQL and fix comments and RAISE NOTICEs

--- functions/make_green_areas.sql
+++ functions/make_green_areas.sql
@@ -1,6 +1,12 @@
+-- make_green_areas.sql
+-- Identifies which paths lie inside green areas and labels them accordingly
+-- Copyright (c) 2024 Jacopo Maltagliati (j.maltagliati@campus.unimib.it)
+-- This file is part of the MUSA micromobility project
+
 CREATE OR REPLACE PROCEDURE public.make_green_areas(IN max_tag_id integer)
- LANGUAGE plpgsql
+LANGUAGE plpgsql
 AS $procedure$
+
 DECLARE
 this_id bigint;
 this_geom geometry;
@@ -14,37 +20,40 @@ DROP TABLE IF EXISTS green_areas;
 CREATE TABLE green_areas (cluster_id serial, ids bigint[], geom geometry);
 CREATE INDEX ON green_areas USING GIST(geom);
 
--- Iterate through linestrings, assigning each to a cluster (if there is an intersection)
--- or creating a new cluster (if there is not)
--- [jm] Only include ways with tag_id < 3000 (refer to mapconfig.xml)
-FOR this_id, this_geom IN SELECT id, the_geom FROM (SELECT * FROM ways WHERE tag_id < 3000) LOOP
-  -- Look for an intersecting cluster.  (There may be more than one.)
-  SELECT cluster_id FROM green_areas WHERE ST_Intersects(this_geom, green_areas.geom)
-     LIMIT 1 INTO cluster_id_match;
+-- Iterate through linestrings, assigning each to a cluster if there is an 
+-- intersection, or creating a new cluster if there is none
+-- XXX [jm] Only include ways with tag_id < 3000 (refer to mapconfig.xml)
+FOR this_id, this_geom IN 
+    SELECT id, the_geom FROM (SELECT * FROM ways WHERE tag_id < 3000) 
+    LOOP
+    -- Look for an intersecting cluster.  (There may be more than one.)
+    SELECT cluster_id FROM green_areas 
+        WHERE ST_Intersects(this_geom, green_areas.geom)
+        LIMIT 1 INTO cluster_id_match;
 
-  IF cluster_id_match IS NULL THEN
-     -- Create a new cluster
-     INSERT INTO green_areas (ids, geom) VALUES (ARRAY[this_id], this_geom);
-  ELSE
-     -- Append line to existing cluster
-     UPDATE green_areas SET geom = ST_Union(this_geom, geom),
-                          ids = array_prepend(this_id, ids)
-      WHERE green_areas.cluster_id = cluster_id_match;
-  END IF;
+    IF cluster_id_match IS NULL THEN
+        -- Create a new cluster
+        INSERT INTO green_areas (ids, geom) VALUES (ARRAY[this_id], this_geom);
+    ELSE
+        -- Append line to existing cluster
+        UPDATE green_areas SET geom = ST_Union(this_geom, geom),
+                            ids = array_prepend(this_id, ids)
+        WHERE green_areas.cluster_id = cluster_id_match;
+    END IF;
 END LOOP;
 
--- Iterate through the green_areas, combining green_areas that intersect each other
+-- Iterate through the green_areas, combining them if they intersect
 LOOP
     SELECT a.cluster_id, b.cluster_id FROM green_areas a, green_areas b 
-     WHERE ST_Intersects(a.geom, b.geom)
-       AND a.cluster_id < b.cluster_id
-      INTO id_a, id_b;
+        WHERE ST_Intersects(a.geom, b.geom) AND a.cluster_id < b.cluster_id
+        INTO id_a, id_b;
 
     EXIT WHEN id_a IS NULL;
     -- Merge cluster A into cluster B
-    UPDATE green_areas a SET geom = ST_Union(a.geom, b.geom), ids = array_cat(a.ids, b.ids)
-      FROM green_areas b
-     WHERE a.cluster_id = id_a AND b.cluster_id = id_b;
+    UPDATE green_areas a 
+        SET geom = ST_Union(a.geom, b.geom), ids = array_cat(a.ids, b.ids)
+        FROM green_areas b
+        WHERE a.cluster_id = id_a AND b.cluster_id = id_b;
 
     -- Remove cluster B
     DELETE FROM green_areas WHERE cluster_id = id_b;
@@ -56,4 +65,3 @@ DELETE FROM green_areas WHERE geom IS NULL;
 ALTER TABLE green_areas DROP COLUMN ids;
 END;
 $procedure$
-
